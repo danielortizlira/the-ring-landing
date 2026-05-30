32dea70f4d6e4a49ff30ea80f67bd50de369403c
Daniel Ortiz <dortizlira@gmail.com>
2026-05-30 09:48:15 -0300

    chore: centralize app name, company name and support email

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def app_name      = Brand::APP_NAME
+  def company_name  = Brand::COMPANY_NAME
+  def support_email = Brand::SUPPORT_EMAIL
 end
---
+++ config/initializers/brand.rb
@@ -0,0 +1,5 @@
+module Brand
+  APP_NAME      = "El Ring"
+  COMPANY_NAME  = "The Ring EAS"
+  SUPPORT_EMAIL = "support@the-ring.app"
+end
